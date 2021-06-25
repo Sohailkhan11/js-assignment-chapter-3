46a85acc721b3bf5fbcd1719936780b4b3d1593a
Sohail Ahmed <sohailalijamali3@gmail.com>
2021-06-25 14:27:02 -0700

    js-assignment-chpter-3

---
+++ app.js
@@ -0,0 +1,26 @@
+var my_age
+
+my_age=prompt("Enter age")
+my_age=alert(" I am  " + my_age  + "  years old")
+
+var count
+
+count=prompt("Enter visit time")
+count=alert(" you are  " + count  + "  time visiting my page")
+
+var birth
+
+birth=prompt("Enter Birth Year ")
+birth=alert("My Birth year is  " + birth  +  "\n" + "\n" + "data type of declered value is number" )
+
+var username
+var product
+var Quantity
+username=prompt("Enter user name  ")
+Quantity=prompt(" Enter Quantity OF Product ")
+product=prompt(" Enter product Title  ")
+
+username=document.write(username + "  ordered " )
+Quantity=document.write(Quantity   +   " "   )
+product=document.write(product + " " + " purchase xyz shop "   )
+
